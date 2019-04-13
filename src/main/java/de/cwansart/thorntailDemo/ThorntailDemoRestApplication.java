58959d0f637b8c847234193483d3f51b12111890
Christian Wansart <christian.wansart@openknowledge.de>
2019-04-13 21:32:27 +0200

    first commit

---
+++ src/main/java/de/cwansart/thorntailDemo/ThorntailDemoRestApplication.java
@@ -0,0 +1,12 @@
+package de.cwansart.thorntailDemo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+
+public class ThorntailDemoRestApplication extends Application {
+}
